5185bb4927f2a94a9a2e8280da7301438f961f37
aith <ariiramanesh@gmail.com>
2021-04-21 22:28:52 -0700

    added implicit sorting to the ready threads list & semaphores popping highest priority threads on awake

--- lab2_priority_scheduling/pintos/src/threads/semaphore.c
+++ lab2_priority_scheduling/pintos/src/threads/semaphore.c
@@ -1,8 +1,8 @@
-/* 
+/*
  * This file is derived from source code for the Pintos
  * instructional operating system which is itself derived
- * from the Nachos instructional operating system. The 
- * Nachos copyright notice is reproduced in full below. 
+ * from the Nachos instructional operating system. The
+ * Nachos copyright notice is reproduced in full below.
  *
  * Copyright (C) 1992-1996 The Regents of the University of California.
  * All rights reserved.
@@ -27,76 +27,74 @@
  * PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
  * MODIFICATIONS.
  *
- * Modifications Copyright (C) 2017-2021 David C. Harrison. 
+ * Modifications Copyright (C) 2017-2021 David C. Harrison.
  * All rights reserved.
  */
 
 #include <stdio.h>
 #include <string.h>
 
-#include "threads/semaphore.h"
 #include "threads/interrupt.h"
+#include "threads/semaphore.h"
 #include "threads/thread.h"
 
-/* 
+/*
  * Initializes semaphore SEMA to VALUE.  A semaphore is a
  * nonnegative integer along with two atomic operators for
  * manipulating it:
  *
- * - down or Dijkstra's "P": wait for the value to become positive, 
+ * - down or Dijkstra's "P": wait for the value to become positive,
  * 	then decrement it.
- * - up or Dijkstra's "V": increment the value(and wake up one waiting 
- * 	thread, if any). 
+ * - up or Dijkstra's "V": increment the value(and wake up one waiting
+ * 	thread, if any).
  */
-void semaphore_init(struct semaphore *sema, unsigned value)
-{
+void semaphore_init(struct semaphore *sema, unsigned value) {
   ASSERT(sema != NULL);
 
   sema->value = value;
   list_init(&sema->waiters);
 }
 
-/* 
- * Down or Dijkstra's "P" operation on a semaphore.  Waits for SEMA's 
+/*
+ * Down or Dijkstra's "P" operation on a semaphore.  Waits for SEMA's
  * value to become positive and then atomically decrements it.
  *
  * This function may sleep, so it must not be called within an
  * interrupt handler.  This function may be called with
  * interrupts disabled, but if it sleeps then the next scheduled
- * thread will probably turn interrupts back on. 
+ * thread will probably turn interrupts back on.
  */
-void semaphore_down(struct semaphore *sema)
-{
+void semaphore_down(struct semaphore *sema) {
   ASSERT(sema != NULL);
   ASSERT(!intr_context());
 
   enum intr_level old_level = intr_disable();
-  while (sema->value == 0)
-  {
-    list_push_back(&sema->waiters, &thread_current()->sharedelem);
-    thread_block();
+  while (sema->value == 0) { // Busy-wait sleep
+    // list_push_back(&sema->waiters, &thread_current()->sharedelem);
+    list_insert_ordered(&sema->waiters, &thread_current()->sharedelem,
+                        thread_priority_gt, NULL);
+    thread_block(); /* Turn off the thread running this code. Upon reawaking,
+                    run loop again. */
   }
   sema->value--;
   intr_set_level(old_level);
 }
 
-/* 
+/*
  * Up or Dijkstra's "V" operation on a semaphore.  Increments SEMA's value
  * and wakes up one thread of those waiting for SEMA, if any.
  *
- * This function may be called from an interrupt handler. 
+ * This function may be called from an interrupt handler.
  */
-void semaphore_up(struct semaphore *semaphore)
-{
+void semaphore_up(struct semaphore *semaphore) {
   enum intr_level old_level;
 
   ASSERT(semaphore != NULL);
 
   old_level = intr_disable();
-  if (!list_empty(&semaphore->waiters))
-  {
-    thread_unblock(list_entry(
-      list_pop_front(&semaphore->waiters), struct thread, sharedelem));
+  if (!list_empty(&semaphore->waiters)) {
+    thread_unblock(list_entry(list_pop_front(&semaphore->waiters),
+                              struct thread, sharedelem));
   }
   semaphore->value++;
   intr_set_level(old_level);
