12817de534ea0b9df32b9fcbf027b2cd433bd289
aith <ariiramanesh@gmail.com>
2021-04-21 20:35:16 -0700

    added lab2 base

--- lab1_timer_sleep/pintos/src/threads/build/devices/timer.d
+++
@@ -1,6 +0,0 @@
-devices/timer.o: ../../devices/timer.c ../../lib/debug.h \
- ../../lib/inttypes.h ../../lib/stdint.h ../../lib/round.h \
- ../../lib/stdio.h ../../lib/stdarg.h ../../lib/stdbool.h \
- ../../lib/stddef.h ../../lib/kernel/stdio.h ../../devices/pit.h \
- ../../devices/timer.h ../../threads/barrier.h ../../threads/interrupt.h \
- ../../threads/thread.h ../../lib/kernel/list.h
